fe14670fb1799ed6e1af79ac7436b1050c9c10e1
Kevin Riggle <kevinr@bestpractical.com>
2006-08-23 22:45:59 +0000

    Fixed what we were passing to Attach

--- lib/RT/Interface/Email/Filter/TakeAction.pm
+++ lib/RT/Interface/Email/Filter/TakeAction.pm
@@ -666,7 +666,7 @@ sub _ReportResults {
         Subject     => "Extended mailgate error",
         Explanation => $msg,
         MIMEObj     => $args{'Message'},
-        Attach      => $args{'Message'},
+        Attach      => $args{'Message'}->as_string,
     );
     return;
 }
