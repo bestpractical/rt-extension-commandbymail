ee4e0819af1a1100835431b148036608ec4b5f2c
Kevin Riggle <kevinr@bestpractical.com>
2006-08-23 22:45:19 +0000

    Trivial

--- t/utils.pl
+++ t/utils.pl
@@ -6,7 +6,7 @@ use warnings;
 
 BEGIN {
 ### after:     push @INC, qw(@RT_LIB_PATH@);
-    push @INC, qw(/opt/rt3/local/lib /opt/rt3/lib);
+    push @INC, qw(/opt/rt3/local/lib /opt/rt3/lib/);
 }
 
 use RT;
