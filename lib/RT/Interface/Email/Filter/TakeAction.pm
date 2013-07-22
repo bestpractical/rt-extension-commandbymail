bestpractical/rt-extension-commandbymail

commit 33d023989f6a96c3280679cbce3e83be29c54252

get rid of uninit warnings

lib/RT/Interface/Email/Filter/TakeAction.pm
@@ -201,6 +201,8 @@ sub GetCurrentUser {
     foreach my $line (@content) {
         next if $line =~ /^\s*$/ && ! $found_pseudoheaders;
         last if $line !~ /^(?:(\S+(?:{.*})?)\s*?:\s*?(.*)\s*?|)$/;
+        next unless defined $1;
+
         $found_pseudoheaders = 1;
         push( @items, $1 => $2 );
         $RT::Logger->debug("Found pseudoheader: $1 => $2");

commit 0ef609828ad9f0618220e486767d893b16aa1f02

Stop parsing headers when we stop finding them
Fixes a regression introduced by 33d0239 which caused t/create.t to
start failing (on line 88).

lib/RT/Interface/Email/Filter/TakeAction.pm
@@ -222,7 +222,8 @@ sub GetCurrentUser {
     foreach my $line (@content) {
         next if $line =~ /^\s*$/ && ! $found_pseudoheaders;
         last if $line !~ /^(?:(\S+(?:{.*})?)\s*?:\s*?(.*)\s*?|)$/;
-        next unless defined $1;
+        last if not defined $1 and $found_pseudoheaders;
+        next if not defined $1;
 
         $found_pseudoheaders = 1;
         push( @items, $1 => $2 );