fbc862fca9d8792fa2a2c36bbc151869abe91e6c
Jim Brandt <jbrandt@bestpractical.com>
2016-04-11 15:01:00 -0400

    Prep for version 2.01

--- lib/RT/Extension/CommandByMail.pm
+++ lib/RT/Extension/CommandByMail.pm
@@ -11,7 +11,7 @@ our @LINK_ATTRIBUTES    = qw(MemberOf Parents Members Children
             HasMember RefersTo ReferredToBy DependsOn DependedOnBy);
 our @WATCHER_ATTRIBUTES = qw(Requestor Cc AdminCc);
 
-our $VERSION = '2.00';
+our $VERSION = '2.01';
 
 =head1 NAME
 
