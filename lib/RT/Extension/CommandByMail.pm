810bdf4938d38b1d933755bb4e0852dc09064a18
Dustin Graves <dustin@bestpractical.com>
2016-08-12 19:12:00 +0000

    remove error for unknown commands and add config setting $CommandByMailErrorOnUnknown to restore default behavior

--- lib/RT/Extension/CommandByMail.pm
+++ lib/RT/Extension/CommandByMail.pm
@@ -33,6 +33,14 @@ Works with RT 4.0, 4.2, 4.4
 
     The comment/reply text goes here
 
+=head1 IMPORTANT
+
+For users of versions of this extension prior to 3.0: Please note that now you
+will not receive an unknown command error email for unknown commands. There
+will be a warning in the logs whenever an unknown command is encountered. A
+setting was added to restore the previous behavior. See the setting
+C<$CommandByMailErrorOnUnknown> under "Configuration" for more information.
+
 =head1 INSTALLATION
 
 =over
@@ -105,6 +113,12 @@ as well.  For example:
 
 If set, the body will not be examined, only the header defined by the previous
 configuration option.
+
+=head2 C<$CommandByMailErrorOnUnknown>
+
+Prior to 2.02, this extension throws an error if it finds an unknown command.
+This is no longer the case. Setting this option will restore that legacy
+behavior.
 
 =head1 CAVEATS
 
@@ -376,6 +390,11 @@ sub ProcessCommands {
 
     foreach my $cmd ( keys %cmds ) {
         my ($val, $msg) = _CheckCommand( $cmd );
+        if ($val == 2) {
+            delete $cmds{$cmd};
+            warn "Skipping unknown command '$cmd'";
+            next;
+        }
         unless ( $val ) {
             $results{ $cmd } = {
                 value   => delete $cmds{ $cmd },
@@ -867,6 +886,7 @@ sub _CheckCommand {
         return 1 if grep $cmd eq lc $_, @LINK_ATTRIBUTES, @WATCHER_ATTRIBUTES;
     }
 
+    return 2 unless RT->Config->Get('CommandByMailErrorOnUnknown');
     return (0, "Command '$cmd' is unknown");
 }
 
