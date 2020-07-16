3e9523ed3b80d97305148f69ed486c38f8c3e495
sunnavy <sunnavy@bestpractical.com>
2020-07-17 03:46:58 +0800

    Update copyright year to 2020

--- lib/RT/Extension/CommandByMail.pm
+++ lib/RT/Extension/CommandByMail.pm
@@ -979,7 +979,7 @@ or via the web at
 
 =head1 LICENSE AND COPYRIGHT
 
-This software is Copyright (c) 2014 by Best Practical Solutions
+This software is Copyright (c) 2014-2020 by Best Practical Solutions
 
 This is free software, licensed under:
 
